3e7c0044674d0e040569269a9cb87b85761db6ce
dammy <hemmanueldami@gmail.com>
2024-01-14 06:28:41 +0100

    Navbar Adjusted

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alpha Powerbase - Admin Dashboard</title>
+    <title>Global Resources Limited - Admin Dashboard</title>
     <link rel="stylesheet" href="style.css?v=1">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
