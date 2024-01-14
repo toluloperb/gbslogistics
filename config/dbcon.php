803f4a44d443139981423fd4787b8ecb2324be49
dammy <hemmanueldami@gmail.com>
2024-01-14 06:16:26 +0100

    Config Updated for Live

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,14 +1,14 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "gbs_data";
-
     // $host = "localhost";
-    // $username = "alphkizf_alphkizf";
-    // $password = "Ipk]dLws-*=q";
-    // $database = "alphkizf_powerbase";
+    // $username = "root";
+    // $password = "";
+    // $database = "gbs_data";
+
+    $host = "localhost";
+    $username = "globrvfu_gbs";
+    $password = "Trodpen2024*";
+    $database = "globrvfu_gbs";
 
     $con = mysqli_connect($host, $username, $password, $database);
 
